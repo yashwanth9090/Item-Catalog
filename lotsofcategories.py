41a36a336edc04d08c37b7b12a5241223dc7694c
Yashwanth Manchikatla <yashwanth.manchikatla@gmail.com>
2017-12-11 17:35:36 -0600

    Update lotsofcategories.py

--- lotsofcategories.py
+++ lotsofcategories.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Category, Base, CategoryItem, User
 
-create_engine("postgresql://catalog:topsecret@localhost/catalogdb")
+engine = create_engine("postgresql://catalog:topsecret@localhost/catalogdb")
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
